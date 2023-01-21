746cc4426853d136999f559378513489fa232589
Shishi <37541779+Shishi42@users.noreply.github.com>
2023-01-21 20:24:39 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
 const slashcommands_loader = require("../loaders/slashcommands_loader")
 const Sequelize = require("sequelize")
-const Discord = require('discord.js')
 
 module.exports = async bot => {
 
@@ -29,25 +28,4 @@ module.exports = async bot => {
   console.log(`Connecté en tant que ${bot.user.tag}!`)
 
   bot.user.setPresence({activities: [{ name: "Holy Ground", type: 2 }], status: 'online'})
-  
-  bot.guilds.cache.forEach(guild => {
-    embed = new Discord.EmbedBuilder()
-    .setColor(bot.color)
-    .setTitle(`The bot has joined : ${guild.name}.`)
-    .setThumbnail(guild.iconURL())
-    .setDescription("Server informations :")
-    .addFields(
-      {name: "Name", value: guild.name},
-      {name: "Server ID", value: guild.id},
-      {name: "Owner", value: guild.members.cache.get(guild.ownerId).user.tag},
-      {name: "Owner ID", value: guild.ownerId},
-      {name: "Creation Date", value: String(guild.createdAt)},
-      {name: "Member Count", value: String(guild.memberCount)},
-      {name: "Is partner ?", value: String(guild.partnered)},
-      {name: "Is verified ?", value: String(guild.verified)},
-    )
-    .setTimestamp()
-    .setFooter({text: 'a BOT by @shishi4272', iconURL: 'https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png'})
-    bot.guilds.cache.get(bot.log_guild).channels.cache.get(bot.log_channel).send({embeds: [embed]})
-  })
 }
