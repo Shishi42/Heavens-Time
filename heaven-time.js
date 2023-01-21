f39b1d226e24771311244d1feaeb07a86e5cdacd
Shishi <37541779+Shishi42@users.noreply.github.com>
2023-01-21 20:17:08 +0100

    Update heaven-time.js

--- heaven-time.js
+++ heaven-time.js
@@ -29,28 +29,3 @@ let auto_update = new cron.CronJob('00 00 * * * *', () => {
 })
 
 auto_update.start()
-
-
-temp = []
-
-for(guild in bot.guilds.cache){
-  embed = new Discord.EmbedBuilder()
-  .setColor(bot.color)
-  .setTitle(`The bot has joined : ${guild.name}.`)
-  .setThumbnail(guild.iconURL())
-  .setDescription("Server informations :")
-  .addFields(
-    {name: "Name", value: guild.name},
-    {name: "Server ID", value: guild.id},
-    {name: "Owner", value: guild.members.cache.get(guild.ownerId).user.tag},
-    {name: "Owner ID", value: guild.ownerId},
-    {name: "Creation Date", value: String(guild.createdAt)},
-    {name: "Member Count", value: String(guild.memberCount)},
-    {name: "Is partner ?", value: String(guild.partnered)},
-    {name: "Is verified ?", value: String(guild.verified)},
-  )
-  .setTimestamp()
-  .setFooter({text: 'a BOT by @shishi4272', iconURL: 'https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png'})
-  temp.push(embed)
-}
-bot.guilds.cache.get(bot.log_guild).channels.cache.get(bot.log_channel).send({embeds: temp})
